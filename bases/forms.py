2b0dc7df76075e3b9338b04d619593b92b6c08b6
AndrewDRichter <andrewrichter1@hotmail.com>
2021-04-16 14:41:46 -0300

    Adicionados forms estado e cidade

--- bases/forms.py
+++ bases/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Pais
+from .models import Pais, Estado, Cidade
 
 
 class PaisForm(forms.ModelForm):
@@ -15,4 +15,27 @@ class PaisForm(forms.ModelForm):
             self.fields[field].widget.attrs.update({'class':'form-control'})
 
 
-    
+class EstadoForm(forms.ModelForm):
+    class Meta:
+        model=Estado
+        fields=['estado', 'ativo']
+        labels={'estado':'Estado', 'ativo':'Ativo'}
+        widget={'estado':forms.TextInput}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({'class':'form-control'})
+
+
+class CidadeForm(forms.ModelForm):
+    class Meta:
+        model=Cidade
+        fields=['cidade', 'ativo']
+        labels={'cidade':'Cidade', 'ativo':'Ativo'}
+        widget={'cidade':forms.TextInput}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({'class':'form-control'})
